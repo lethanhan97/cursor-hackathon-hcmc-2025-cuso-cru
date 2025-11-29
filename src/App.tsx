39e4e533750f78660be9a7608a4e6846503319ce
Le Thanh An <lethanhanzzz@gmail.com>
2025-11-29 14:31:55 +0700

    feat: add mood detection based on face

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
 import { useRef, useState } from "react";
 import "./App.css";
+import * as faceapi from "@vladmandic/face-api";
+
+await faceapi.nets.ssdMobilenetv1.loadFromUri("/model");
+await faceapi.nets.faceExpressionNet.loadFromUri("/model");
+
+type Mood =
+  | "angry"
+  | "disgusted"
+  | "fearful"
+  | "happy"
+  | "neutral"
+  | "sad"
+  | "surprised";
 
 function App() {
   const localVideoRef = useRef<
     HTMLVideoElement & { srcObject: MediaStream | null }
   >(null);
+  const moodIntervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  const [mood, setMood] = useState<Mood | null>(null);
   const [isStreaming, setIsStreaming] = useState(false);
+
+  const getMood = async () => {
+    if (!localVideoRef.current) return;
+    const detections = await faceapi
+      .detectAllFaces(localVideoRef.current)
+      .withFaceExpressions();
+
+    if (detections) {
+      const moods = detections.map(({ expressions }) => {
+        return expressions.asSortedArray()[0];
+      });
+
+      const mostLikelyMood = moods.sort(
+        (a, b) => b.probability - a.probability
+      )[0];
+      setMood(mostLikelyMood?.expression ?? "neutral");
+    }
+
+    return null;
+  };
 
   const start = async () => {
     const media = await navigator.mediaDevices.getUserMedia({
@@ -17,6 +53,7 @@ function App() {
       localVideoRef.current.srcObject = media;
     }
 
+    setInterval(getMood, 1000);
     setIsStreaming(true);
   };
   const stop = async () => {
@@ -29,6 +66,11 @@ function App() {
       localVideoRef.current.srcObject = null;
     }
     setIsStreaming(false);
+
+    if (moodIntervalRef.current) {
+      clearInterval(moodIntervalRef.current);
+      moodIntervalRef.current = null;
+    }
   };
 
   const toggle = () => {
@@ -49,6 +91,7 @@ function App() {
         playsInline
         className="preview"
       />
+      {mood && <p>Mood: {mood}</p>}
       <button onClick={toggle}>
         {isStreaming ? "Stop Streaming" : "Start Streaming"}
       </button>
