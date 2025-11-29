bd77f7bf077a725876f33e6943be35ea906fb527
Le Thanh An <lethanhanzzz@gmail.com>
2025-11-29 14:15:08 +0700

    feat: add base app with video stream

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useRef } from "react";
 import "./App.css";
 
 function App() {
+  const localVideoRef = useRef<HTMLVideoElement>(null);
+
+  const start = async () => {
+    const media = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+
+    if (localVideoRef.current) {
+      localVideoRef.current.srcObject = media;
+    }
+  };
   return (
-    <section>
-      <h1>Bg Muzik generator</h1>
+    <section className="app">
+      <h1>Bg Muzike CUSO CRU 2025 SUPER COOL</h1>
+      <video
+        ref={localVideoRef}
+        autoPlay
+        muted
+        playsInline
+        className="preview"
+      />
+      <button onClick={start}>Start WebRTC</button>
     </section>
   );
 }
